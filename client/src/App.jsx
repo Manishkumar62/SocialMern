2ccc51d79f77467d8bb6d120c3833cee7336143a
Manishkumar62 <vishwamanish62@gmail.com>
2023-08-24 17:11:58 +0530

    login,sign,posts,likes,friends

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+function App() {
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "/home",
+      element: isAuth ? <HomePage /> : < Navigate to="/" />,
+    },
+    {
+      path: "/profile/:userId",
+      element: isAuth ? <ProfilePage /> : < Navigate to="/" />,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from "./Form";
+
+const LoginPage = () => {
+
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const handleNotRegistered = () => {
+    alert("You are not registered; please register to use the app");
+  };
+
+  return (
+    <Box>
+      <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          SocialMern
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500%" variant='h5' sx={{ mb: "1.5rem" }}>
+          Welcome to SocialMern, the Social Media for Socialpath!
+        </Typography>
+        <Form onNotRegistered={handleNotRegistered} /> {/* Pass the handleNotRegistered function as a prop */}
+      </Box>
+    </Box>
+  )
+}
+
+export default LoginPage
